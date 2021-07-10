01527792ff2ac9a6af41d7b718bf04fec94b81a9
raymondheil <68617358+raymondheil@users.noreply.github.com>
2021-07-09 20:47:47 -0600

    Herd-based prey placement, basis of agent-based modeling

--- agent_based/pred_prey.py
+++ agent_based/pred_prey.py
@@ -1,1 +1,50 @@
 #! usr/bin/python3
+
+from mesa import Agent, Model
+from mesa.space import ContinuousSpace
+from mesa.time import RandomActivation
+# from mesa.datacollection import DataCollector
+# from mesa.batchrunner import BatchRunner
+
+class PreyAgent(Agent):
+    """A prey agent who herds with other prey 
+    and is attacked by predators"""
+    def __init__(self, unique_id, model, x, y):
+        super().__init__(unique_id, model)
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("Prey %s moves." % (self.unique_id))
+
+    def step(self):
+        self.move()
+
+
+class PredatorAgent(Agent):
+    """A predator agent who attacks prey."""
+    def __init__(self, unique_id, model, x, y):
+        super().__init__(unique_id, model)
+        self.x = x
+        self.y = y
+        
+    def move(self):
+        print("Predator %s moves." % (self.unique_id))
+
+    def step(self):
+        self.move()
+
+
+class HerdModel(Model):
+    def __init__(self, N_predator, N_prey, width, height):
+        self.num_predator = N_predator
+        self.num_prey = N_prey
+        self.space = ContinuousSpace(width, height, True)
+        self.schedule = RandomActivation(self)
+        self.make_agents()
+
+    def make_agents(self):
+        for i in range(self.num_predator):
+            print("Predator %s placed" % i)
+        for i in range(self.num_prey):
+            print("Prey %s placed" % i)
--- prey_distribution/prey_distribution_montecarlo.py
+++ prey_distribution/prey_distribution_montecarlo.py
@@ -1,6 +1,23 @@
 #! /usr/bin/env python3
 
-## IMPORTS ##
+'''
+    $NAME: Agent-based Monte Carlo modeling of herding dynamics.
+    Copyright (C) 2021 Raymond Heil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 
 import math
 import random
@@ -11,12 +28,12 @@ import numpy as np
 import scipy.stats as stats
 import pandas as pd
 
+
 for term in sys.argv:
-    """
-    Read in arguments given in the command line in order to customize program.
+    """Read in arguments given in the command line in order to customize program.
     Expandable to other options, including the possibility of specific file input
-    for configuration or other uses.
-    """
+    for configuration or other uses."""
+
     if term == "--quiet" or term == "-q": # verbose output is on by default
         verbose = False
     else:
@@ -28,11 +45,13 @@ for term in sys.argv:
 
 
 def main():
-    fname = 'prey-distribution.conf'
+    fname = 'prey_distribution.conf'
     config = read_config(fname)
-    if verbose: # interactive prompt to confirm config info, prompting user to change config if info is incorrect
-        print('Preparing to place %i herds of average size %i on a %ix%i field.' % (config['herd-number'], config['average-herd-size'], config['width'], config['height']))
-        if not accept_all:
+    # interactive prompt to confirm config info, prompting user to change config if info is incorrect
+    if verbose: 
+        print('Preparing to place %i herds of average size %i on a %ix%i field.'
+              % (config['herd-number'], config['average-herd-size'], config['width'], config['height']))
+        if not accept_all: # just skip this section if the user specified -y
             correct = input("Is this information correct? [Y/n]: ").lower()
             if correct == "n" or correct == "no": # abort if user answered n or no, continue otherwise
                 print('You can update the configuration and correct this information in prey-distribution.cfg.')
@@ -67,7 +86,7 @@ def main():
             y = position[1]
             herd_x = herd_position[0]
             herd_y = herd_position[1]
-            cursor.execute("INSERT INTO prey_data(x, y, herd_x, herd_y) VALUES (?, ?, ?, ?)", (x, y, herd_x, herd_y)) # FIXME: this is calling at the wrong time, herd_x is always the same LAST VALUE
+            cursor.execute("INSERT INTO prey_data(x, y, herd_x, herd_y) VALUES (?, ?, ?, ?)", (x, y, herd_x, herd_y))
         # print(cursor.execute("SELECT x, y, herd_x, herd_y FROM prey_data").fetchall())
         sql_query = cursor.execute("SELECT x, y FROM prey_data").fetchall()
         df = pd.DataFrame(sql_query)
@@ -75,13 +94,12 @@ def main():
 
 
 def read_config(fname):
-    """
-    Read a config file into the program, separating terms and their values
+    """Read a config file into the program, separating terms and their values
     and adding them to a dictionary that can be referenced later.
 
     In the future, read_file will also perform checks and cross-checks that values
-    are logical and point the user in the direction of any errors.
-    """
+    are logical and point the user in the direction of any errors."""
+    
     config = {}
     f = open(fname, 'r')
     for line in f.readlines():
@@ -98,20 +116,27 @@ def read_config(fname):
 
 
 def place_herd(herd_position, member_number, config):
-    """
-    Place a full herd of individuals, based around a centerpoint.
-    """
+    """Place a full herd of individuals, based around a centerpoint."""
+    
     member_positions = []
     for member in range(member_number):
-        rotation = (random.random() * 2 * math.pi) # rotation stored in radians
-        distance = abs(random.gauss(0, config['herd-spacing-stdev'])) # absolute value of gaussian distribution means we have everything over the mean, so 66% of members are within 1 stdev of the center.
-        herd_x = herd_position[0]
-        herd_y = herd_position[1]
-        # TODO: add weighting and check multiple possible points.
-        # REVIEW: will using the distribution and wieghting together bias the placement?
-        member_x = int((math.sin(rotation) * distance) + herd_x) # REVIEW: should I be using floats or ints? this does ints right now but I could change it easily enough
-        member_y = int((math.cos(rotation) * distance) + herd_y)
-
+        # REVIEW: will using the distribution and wieghting together bias the placement at all? how?
+        if member_positions:
+            distance_value = {}
+            value_distribution = stats.norm(25, 10) # TODO: make these parameters available in config (25, 3) was old
+            for i in range(int(config['individual-placement-attempts'])):
+                (test_x, test_y) = new_random_position(herd_position, config['herd-spacing-stdev'])
+                point_value = 0
+                for other_position in member_positions:
+                    other_x, other_y = other_position
+                    distance_to_other = math.sqrt((test_x - other_x)**2 + (test_y - other_y)**2)
+                    point_value += value_distribution.pdf(distance_to_other) # the weight of this term in the final selection
+                    print('point_value %.4f for other_position %s test_position (%s, %s) and i %s' % (point_value, other_position, test_x, test_y, i))
+            distance_value[(test_x, test_y)] = point_value
+            member_x, member_y = random.choices(list(distance_value.keys()), list(distance_value.values()), k=1)[0] # janky but it comes out as a tuple in a list
+            # print('x:%s\ty:%s' % (member_x, member_y)) 
+        else: # if there aren't already entries, just place yourself at a random point point
+            member_x, member_y = new_random_position(herd_position, config['herd-spacing-stdev']) 
         # wrap around values outside the limits of the model, creating a toroidal space
         if 0 > member_x or member_x > config['width']:
             member_x = member_x % config['width']
@@ -122,7 +147,17 @@ def place_herd(herd_position, member_number, config):
         # print('%i\t%i' % (member_x, member_y)) # bootleg way of getting this to work out of the box with gnuplot
         member_positions.append((member_x, member_y))
     # print(member_positions)
-    return (member_positions) # to be fed back into a dictionary or sql database
+    return(member_positions) # to be fed back into a dictionary or sql database
 
+
+def new_random_position(herd_position, herd_spacing_stdev):
+    rotation = (random.random() * 2 * math.pi) # radians, not degrees
+    distance = abs(random.gauss(0, herd_spacing_stdev))
+    herd_x = herd_position[0]
+    herd_y = herd_position[1]
+    member_x = int((math.sin(rotation) * distance) + herd_x) 
+    member_y = int((math.cos(rotation) * distance) + herd_y)
+    return((member_x, member_y))
+    
 
 main()
