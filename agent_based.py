6db24cc5848e90928855462bd8049e6e163f5113
raymondheil <68617358+raymondheil@users.noreply.github.com>
2021-07-12 13:57:46 -0600

    Folder restructure for easier integration, basis of agent-based behavior

--- agent_based.py
+++ agent_based.py
@@ -18,33 +18,32 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
+import math
 from mesa import Agent, Model
 from mesa.space import ContinuousSpace
 from mesa.time import RandomActivation
 # from mesa.datacollection import DataCollector
 # from mesa.batchrunner import BatchRunner
 
+
 class PreyAgent(Agent):
-    """A prey agent who herds with other prey 
+    """A prey agent who herds with other prey
     and is attacked by predators"""
-    def __init__(self, unique_id, model, x, y):
+    def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.x = x
-        self.y = y
 
     def move(self):
-        print("Prey %s moves." % (self.unique_id))
-
+        # print("Prey %s moves." % (self.unique_id))
+        possible_moves = 
+        
     def step(self):
         self.move()
 
 
 class PredatorAgent(Agent):
     """A predator agent who attacks prey."""
-    def __init__(self, unique_id, model, x, y):
+    def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.x = x
-        self.y = y
         
     def move(self):
         print("Predator %s moves." % (self.unique_id))
@@ -57,12 +56,33 @@ class HerdModel(Model):
     def __init__(self, N_predator, N_prey, width, height):
         self.num_predator = N_predator
         self.num_prey = N_prey
+        self.width = width
+        self.height = height
         self.space = ContinuousSpace(width, height, True)
         self.schedule = RandomActivation(self)
         self.make_agents()
 
-    def make_agents(self):
-        for i in range(self.num_predator):
-            print("Predator %s placed" % i)
-        for i in range(self.num_prey):
-            print("Prey %s placed" % i)
+
+    def make_agents(self): # this is where I feed in the prey placement code and also add RANDOM predator placement
+        for i in range(self.num_predator + self.num_prey):
+            if i < self.num_predator:
+                a = PredatorAgent(i, self)
+                x = self.random.randrange(self.width)
+                y = self.random.randrange(self.height)
+            else:
+                a = PreyAgent(i, self)
+                x = self.random.randrange(self.width)
+                y = self.random.randrange(self.height)
+            self.schedule.add(a)
+            self.space.place_agent(a, (x, y))
+            # print('%s placed at (%s, %s)' % (type(a), x, y))
+            
+
+    def step(self):
+        self.schedule.step()
+
+
+
+if __name__ == "__main__":
+    main()
+hi all, I thought I would mention something about your public s/w repos on github and other places: you might want to do a bit of a curating pass at them now that you've been at them for a while.  make sure you rename (with "git mv") programs so that their  names make sense, and make sure that your README.md file is very very good
