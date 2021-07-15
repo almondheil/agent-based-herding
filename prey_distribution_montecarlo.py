e7372c6bd027b8edc4340e85eebbe919e730cf5f
raymondheil <68617358+raymondheil@users.noreply.github.com>
2021-07-14 21:33:08 -0600

    Fixed bug with empty distance_value array

--- prey_distribution_montecarlo.py
+++ prey_distribution_montecarlo.py
@@ -45,8 +45,8 @@ def place_herds(config, params):
         herd_x = random.randint(0, config['width'])
         herd_y = random.randint(0, config['height'])
         herd_positions[(herd_x, herd_y)] = member_number
-        if params['verbose']: print('Herd %i to be placed at (%i, %i) with %i members' % (i+1, herd_x, herd_y, member_number))
-    if params['verbose']: print('\nIn total, %i prey individuals will be placed in %i herds\n' %
+        if params['verbose']: print('Herd %i to be placed at (%i, %i) with %i member(s)' % (i+1, herd_x, herd_y, member_number))
+    if params['verbose']: print('\nIn total, %i prey individuals will be placed in %i herds...\n' %
                                 (sum(herd_positions.values()), config['herd-number']))
     return(herd_positions)
 
@@ -70,11 +70,16 @@ def place_herd_members(config, params, herd_position, member_number):
                     point_value += value_distribution.pdf(distance_to_other) # the weight of this term in the final selection
                     # print('point_value %.4f for other_position %s test_position (%s, %s) and i %s' % (point_value, other_position, test_x, test_y, i))
             distance_value[(test_x, test_y)] = point_value
-            distance_value = {key:val for key, val in distance_value.items() if val != 0} # remove any values that are just zero, which trips up random.choices()
-            for value in distance_value.items():
-                if value == 0:
-                    raise Exception("The value of a point was found to be zero, which should mean it was removed from the running. Please let the developer know of this error.")
-            member_x, member_y = random.choices(list(distance_value.keys()), list(distance_value.values()), k=1)[0]
+            distance_value_stripped = {key:val for key, val in distance_value.items() if val != 0} # remove any values that are just zero, which trips up random.choices()
+            if distance_value_stripped: # only use choices() if there ARE any items in the stripped version, as an eptry array from the 0 removal would confuse it
+                member_x, member_y = random.choices(list(distance_value_stripped.keys()), list(distance_value_stripped.values()), k=1)[0]
+            else:
+                member_x, member_y = random.choice(list(distance_value.keys()))
+            # print(list(distance_value.keys()))
+            # print(list(distance_value.values()))
+            # only do this if there IS anything in distance_value. if not, just randomly choose.
+            # 1. don't delete the items if every item in the list is 0
+            # 2. instead, if EVERY ITEM is 0 go ahead and just choose entirely randomly among the points
             # print('x:%s\ty:%s' % (member_x, member_y))
         else: # if there aren't already entries, just place yourself at a random point point
             member_x, member_y = new_random_position(herd_position, config['herd-spacing-stdev'])
@@ -84,7 +89,7 @@ def place_herd_members(config, params, herd_position, member_number):
         if 0 > member_y or member_y > config['height']:
             member_y = int(member_y % config['height'])
         # print('rotation: %s distance: %s x: %s y: %s' % (rotation, distance, member_x, member_y))
-        if params['verbose']: print('# Individual placed at (%i, %i) belonging to herd at (%i, %i)' % (member_x, member_y, herd_x, herd_y))
+        # if params['verbose']: print('# Individual placed at (%i, %i) belonging to herd at (%i, %i)' % (member_x, member_y, herd_x, herd_y))
         # print('%i\t%i' % (member_x, member_y)) # bootleg way of getting this to work out of the box with gnuplot
         member_positions.append((member_x, member_y))
     # print(member_positions)
